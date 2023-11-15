9daee22b48a701841e81d04e9a3dcabb0195105b
Yohan apedo <yohan.apedo@etu.univ-poitiers.fr>
2023-11-15 17:15:29 +0100

    fix NumberOfCharacters

--- src/components/NumberOfCharacters.js
+++ src/components/NumberOfCharacters.js
@@ -1,7 +1,10 @@
 export function NumberOfCharacters({ characters = [] }) {
     return (
-      <p>
-        There is {characters.length === 0 ? 'no' : characters.length} character{characters.length !== 1 ? 's' : ''}
-      </p>
+        <p>
+            There is {characters.length === 0 ? 'no' : characters.length} character{characters.length !== 1 ? 's' : ''}
+        </p>
     );
-  }
+}
+
+
+export default NumberOfCharacters;
