2a0ec93f0122d48b1274bba57124cc6e06b6dd87
Yohan apedo <yohan.apedo@etu.univ-poitiers.fr>
2023-11-15 18:08:53 +0100

    Add sort by and order in character-api

--- src/api/character-api.js
+++ src/api/character-api.js
@@ -4,8 +4,24 @@ const characters = require('../data/characters.json');
  * Get all characters from json file
  * @returns 
  */
-function getCharacters() {
-    return characters
+function getCharacters(orderBy = 'name', order = 'asc') {
+    // Sort characters by name
+    let sortedCharacters = characters.sort((a, b) => {
+        if (orderBy === 'name') {
+            return a.name.localeCompare(b.name)
+        } else if (orderBy === 'modified') {
+            return new Date(b.modified) - new Date(a.modified)
+        } else {
+            throw new Error(`Invalid orderBy parameter: ${orderBy}`)
+        }
+    })
+
+    // Reverse the order if it is descending
+    if (order === 'desc') {
+        sortedCharacters.reverse()
+    }
+
+    return sortedCharacters
 }
 
 /**
